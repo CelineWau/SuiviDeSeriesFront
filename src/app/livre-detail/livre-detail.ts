9b86e8925c9800fb1165795c54d3d51f59ec49f9
CelineWau <ce.waucheul@gmail.com>
2026-09-02 11:23:05 +0200

    Formulaire de modification de livre (LivreDetail) : GET /livres/{id}, formulaire titre/auteur/isbn/numero, navigation retour vers la fiche série

--- src/app/livre-detail/livre-detail.ts
+++ src/app/livre-detail/livre-detail.ts
@@ -2,12 +2,13 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Livre } from '../services/livre';
 import { Serie } from '../services/serie';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { LivreDetailData } from '../models/livre-detail';
+import { ModifierLivreData } from '../models/modifier-livre';
 
 @Component({
   selector: 'app-livre-detail',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './livre-detail.html',
   styleUrl: './livre-detail.css',
 })
@@ -16,8 +17,12 @@ export class LivreDetail implements OnInit{
   idSerie: number;
   idLivre: number;
   livre: LivreDetailData | null = null;
+  nouveauTitre: string = '';
+  nouveauAuteur: string = '';
+  nouveauIsbn: string = '';
+  nouveauNumero: number = 0;
 
-  constructor(private livreService: Livre, private cdr: ChangeDetectorRef, private route: ActivatedRoute, private serieService: Serie) {
+  constructor(private livreService: Livre, private cdr: ChangeDetectorRef, private route: ActivatedRoute, private router: Router, private serieService: Serie) {
     this.idSerie = Number(this.route.snapshot.paramMap.get('serieId'));
     this.idLivre = Number(this.route.snapshot.paramMap.get('livreId'));
   }
@@ -30,7 +35,26 @@ export class LivreDetail implements OnInit{
   chargerLivre(): void {
     this.livreService.getLivre(this.idLivre).subscribe(data => {
       this.livre = data;
+      this.nouveauTitre = data.titre;
+      this.nouveauAuteur = data.auteur;
+      this.nouveauIsbn = data.isbn;
+      this.nouveauNumero = data.numeroDansLaSerie;
       this.cdr.detectChanges();
     });
+  }
+
+  enregistrerModification(): void {
+    const donneesModifiees: ModifierLivreData = {
+      titre: this.nouveauTitre,
+      auteur: this.nouveauAuteur,
+      isbn: this.nouveauIsbn,
+      numeroDansLaSerie: this.nouveauNumero
+    };
+
+    this.livreService.modifierLivre(this.idLivre, donneesModifiees).subscribe(data => {
+      this.livre = data;
+      this.cdr.detectChanges();
+      this.router.navigate(['/series', this.idSerie]);
+    })
   }
 }
